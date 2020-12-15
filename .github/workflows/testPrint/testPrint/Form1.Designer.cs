2f59b86d59db70280163fb0fbd24e4b0c43adffa
OJBK123 <37143745+OJBK123@users.noreply.github.com>
2020-12-15 10:16:34 +0800

    Add files via upload

---
+++ .github/workflows/testPrint/testPrint/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace testPrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtH = new System.Windows.Forms.TextBox();
+            this.txtW = new System.Windows.Forms.TextBox();
+            this.lheight = new System.Windows.Forms.Label();
+            this.lb = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(625, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打印";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(37, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(293, 20);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txtH
+            // 
+            this.txtH.Location = new System.Drawing.Point(688, 232);
+            this.txtH.Multiline = true;
+            this.txtH.Name = "txtH";
+            this.txtH.Size = new System.Drawing.Size(81, 41);
+            this.txtH.TabIndex = 1;
+            // 
+            // txtW
+            // 
+            this.txtW.Location = new System.Drawing.Point(688, 291);
+            this.txtW.Multiline = true;
+            this.txtW.Name = "txtW";
+            this.txtW.Size = new System.Drawing.Size(81, 41);
+            this.txtW.TabIndex = 1;
+            // 
+            // lheight
+            // 
+            this.lheight.AutoSize = true;
+            this.lheight.Location = new System.Drawing.Point(623, 253);
+            this.lheight.Name = "lheight";
+            this.lheight.Size = new System.Drawing.Size(41, 12);
+            this.lheight.TabIndex = 3;
+            this.lheight.Text = "height";
+            // 
+            // lb
+            // 
+            this.lb.AutoSize = true;
+            this.lb.Location = new System.Drawing.Point(623, 304);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(35, 12);
+            this.lb.TabIndex = 3;
+            this.lb.Text = "width";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(640, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "自定义";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(37, 94);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(435, 244);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(640, 16);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 16);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "预览";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(640, 79);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(48, 16);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "横向";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(688, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 365);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.lheight);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtW);
+            this.Controls.Add(this.txtH);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "打印测试小程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtH;
+        private System.Windows.Forms.TextBox txtW;
+        private System.Windows.Forms.Label lheight;
+        private System.Windows.Forms.Label lb;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ .github/workflows/testPrint/testPrint/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Text;
+using System.Windows.Forms;
+ 
+
+namespace testPrint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> listdata = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listdata.Clear();
+                listdata.Add("这是第一张卡片");
+                listdata.Add("这是第二张卡片");
+                //listdata.Add("3");
+                //listdata.Add("4");
+                //listdata.Add("5");
+                //listdata.Add("6");
+                //listdata.Add("7");
+                //listdata.Add("8");
+                if (comboBox1.Text.Trim() != "")
+                    printDocument1.PrinterSettings.PrinterName = comboBox1.Text.Trim();
+                else
+                printDocument1.PrinterSettings.PrinterName = "Zebra P330i Card Printer USB";
+//
+                
+                if (txtW.Text==""||txtH.Text=="")
+                {
+                    MessageBox.Show("输入高宽");
+                    return;
+                }
+                if (checkBox1.Checked)
+                {
+                    printDocument1.DefaultPageSettings.PaperSize = new PaperSize("Custom Size", Convert.ToInt32(txtW.Text), Convert.ToInt32(txtH.Text));
+                }
+                else
+                {
+                    printDocument1.DefaultPageSettings.PaperSize = GetPrintSetting(printDocument1.PrinterSettings.PrinterName,Convert.ToInt32( txtW.Text), Convert.ToInt32( txtH .Text));
+                }
+                if (checkBox3.Checked)
+                {
+                    printDocument1.DefaultPageSettings.Landscape = true;
+                }
+
+               if( MessageBox.Show("选中的纸为" + printDocument1.DefaultPageSettings.PaperSize.PaperName, "提示", MessageBoxButtons.YesNo)==DialogResult.No)
+               {
+                   return;
+               }
+                if(checkBox2.Checked)
+                {
+                    printPreviewDialog1.Document = printDocument1;
+                    printPreviewDialog1.ShowDialog();
+                }
+                else
+                {
+                    printDocument1.Print();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public PaperSize GetPrintSetting(string PrinterName, int width, int height)
+        {
+            foreach (string sPrint in PrinterSettings.InstalledPrinters)
+            {
+                if (sPrint == PrinterName)
+                {
+                    foreach (PaperSize ps in printDocument1.PrinterSettings.PaperSizes)
+                    {
+                        if (Math.Abs(ps.Width - width) < 10)
+                        {
+                            if (Math.Abs(ps.Height - height) < 10)
+                            {
+                                return ps;
+                            }
+                        }
+                    }
+                }
+            }
+            return new PaperSize("Custom Size", width, height);
+        }
+        public void GetPrintSetting(string PrinterName)
+        {
+            listBox1.Items.Clear();
+            if (PrinterName!="")
+            {
+                printDocument1.PrinterSettings.PrinterName = PrinterName;
+            }
+            foreach (string sPrint in PrinterSettings.InstalledPrinters)
+            {
+                if (sPrint == PrinterName)
+                {
+                    foreach (PaperSize ps in printDocument1.PrinterSettings.PaperSizes)
+                    {
+                         listBox1.Items.Add(string.Format("\r\n PaperName{2} PaperKind{3} width{0} height{1} _{0}_{1}", ps.Width, ps.Height,ps.PaperName,ps.RawKind.ToString()));
+                    }
+                }
+            }
+        }
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            try
+            {
+                int xx = 0;
+                if (textBox1.Text!="")
+                {
+                    xx = Convert.ToInt32(textBox1.Text);
+                }
+                Graphics g = e.Graphics;
+
+                if (listdata.Count > 0)
+                { 
+
+                    Font newft = new Font("宋体", 30, FontStyle.Bold);
+                    g.DrawString(listdata[0], newft, System.Drawing.Brushes.Black, 10 + xx, 10 + xx);
+                    g.DrawString(listdata[0], newft, System.Drawing.Brushes.WhiteSmoke, 10 + xx, 50 + xx);
+                    listdata.RemoveAt(0);
+                    if (listdata.Count <= 0)
+                    {
+                        e.HasMorePages = false;
+                        listdata.Add("这是第一张卡片");
+                        listdata.Add("这是第二张卡片");
+                    }
+                    else
+                    {
+                        e.HasMorePages = true;
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBox1.Items.Add("");
+                foreach (var v in   PrinterSettings.InstalledPrinters)
+                {
+                    comboBox1.Items.Add(v);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                 
+                GetPrintSetting(comboBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var v = listBox1.SelectedItem;
+                if(v!=null)
+                {
+                    var dd = v.ToString().Split('_');
+                    txtH.Text = dd[2];
+                    txtW.Text = dd[1];
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
